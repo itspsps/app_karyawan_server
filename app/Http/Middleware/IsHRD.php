b4e2e83c8d089b10e35524060894b4b6fa129915
itspsps <it.dev.sps@gmail.com>
2024-10-15 11:33:25 +0700

    ya

--- app/Http/Middleware/IsHRD.php
+++ app/Http/Middleware/IsHRD.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class IsHRD
 {
@@ -23,8 +24,9 @@ class IsHRD
         }
         // dd(Auth::user());
         if (Auth::user()->is_admin != "hrd") {
-            dd('ok');
-            return redirect()->back();
+            // dd('ok');
+            Alert::warning('warning', 'Auth Access User Anda Diabatasi');
+            return redirect()->back()->with('warning', 'Auth Access User Anda Diabatasi');
         }
         return $next($request);
     }
