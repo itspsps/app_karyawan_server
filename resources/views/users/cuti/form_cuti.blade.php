8da59677ad7eaf5d06bb1c8a53330733e0e22b62
itspsps <it.dev.sps@gmail.com>
2024-10-17 14:34:43 +0700

    keri

--- resources/views/users/cuti/form_cuti.blade.php
+++ resources/views/users/cuti/form_cuti.blade.php
@@ -135,15 +135,15 @@
         <tbody>
             <tr style="font-weight: bold;">
                 <td style="text-align: center;">
-                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/'.$data_cuti->ttd_user.'.png') }}" width="100%" alt="">
+                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/cuti/'.$data_cuti->ttd_user.'.png') }}" width="100%" alt="">
                     <p>{{$user_karyawan->name}}</p>
                 </td>
                 <td style="text-align: center;">
-                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/'.$data_cuti->ttd_atasan.'.png') }}" width="100%" alt="">
+                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/cuti/'.$data_cuti->ttd_atasan.'.png') }}" width="100%" alt="">
                     <p style="margin-bottom: -10px;">{{$data_cuti->approve_atasan}}</p>
                 </td>
                 <td style="text-align: center;">
-                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/'.$data_cuti->ttd_atasan2.'.png') }}" width="100%" alt="">
+                    <img src="{{ url('https://hrd.sumberpangan.store:4430/public/signature/cuti/'.$data_cuti->ttd_atasan2.'.png') }}" width="100%" alt="">
                     <p>{{$data_cuti->approve_atasan2}}</p>
                 </td>
             </tr>
